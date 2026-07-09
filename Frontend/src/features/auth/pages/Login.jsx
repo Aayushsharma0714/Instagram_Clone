7e5e776046c7e5696df97a6f2b7a90e20e5ec224
Aayushsharma0714 <aayushbhardwaj147@gmail.com>
2026-07-09 21:50:26 +0530

    Integration initialize

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React ,  {useState} from 'react'
+import {Link} from 'react-router'
+import axios from 'axios'
+import '../style/form.scss'
+const Login = () =>{ 
 
-const Login = () => {
+  const [username ,setUsername] = useState('')
+  const [password ,setPassword] = useState('')
+
+  async function handleSubmit(e){
+    e.preventDefault()
+
+    axios.post("http://localhost:3000/api/auth/login" , {
+      username, 
+      password
+    } , {
+      withCredentials:true
+    })
+  }
+
   return (
-    <div>Login</div>
+     <main>
+     <div className="form-container">
+       <h2>Login</h2>
+       <form onSubmit={handleSubmit}>
+         <input onInput={(e)=>setUsername(e.target.value)}  
+         type="text"  
+         name='username' 
+         placeholder="Enter your name" />
+         <input onInput={(e)=>setPassword(e.target.value)}  
+         type="password"
+          name='password'
+          placeholder="Password" />
+         <button type="submit">Login</button>
+       </form>
+              <p>Don't have an account? <Link className='toggleAuthForm' to="/register">Register</Link></p>
+     </div>
+     </main>
   )
 }
 
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import {Link} from 'react-router'
+import '../style/form.scss'
+import { useState } from 'react'
+import axios from 'axios'
+
 
 const Register = () => {
+
+const [username ,setUsername] = useState('')
+const [email ,setEmail] = useState('')
+const [password ,setPassword] = useState('')
+
+async function handleSubmit(e){
+  e.preventDefault()
+
+  axios.post('http://localhost:3000/api/auth/register',{
+    username,
+    email,  
+    password
+  } ,{
+    withCredentials:true 
+  }
+)
+  .then((res)=>{
+    console.log(res.data)
+  })
+}
   return (
-    <div>Register</div>
+       <main>
+     <div className="form-container">
+       <h2>Register</h2>
+       <form onSubmit={handleSubmit}>
+         <input onInput={(e)=>{setUsername(e.target.value)}}
+         type="text"
+         name='username' 
+         placeholder="Enter your name" />
+         
+         <input onInput={(e)=>{setEmail(e.target.value)}}
+         type='email'
+          name='email' 
+          placeholder="Enter your email" />
+         
+         <input onInput={(e)=>{setPassword(e.target.value)}}
+         type="password" 
+         name='password'
+          placeholder="Password" />
+         
+         <button type="submit">Register</button>
+       </form>
+       <p>Have a account? <Link className='toggleAuthForm' to="/login">Login</Link></p>
+     </div>
+     </main>
   )
 }
 
